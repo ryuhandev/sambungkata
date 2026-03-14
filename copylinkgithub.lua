90e1a4c3518a8db829f3e824081b74ba2515f08d
Ryuhan Minamoto <165671956+ryuhandev@users.noreply.github.com>
2026-03-14 13:58:05 +0700

    Update and rename copylinkgithub.lua.txt to copylinkgithub.lua

--- copylinkgithub.lua
+++ copylinkgithub.lua
@@ -11,7 +11,7 @@ if CoreGui:FindFirstChild("InformationUpdatePopup") then
 end
 
 -- ================= CONFIG =================
-local WhatsAppLink = "https://www.whatsapp.com/channel/0029VbCBSBOCRs1pRNYpPN0r"
+local WhatsAppLink = "https://whatsapp.com/channel/0029Vb7tSbFLikgCMYq0912t"
 local AccentColor = Color3.fromRGB(255, 255, 255)
 local BgColor = Color3.fromRGB(15, 15, 15)
 
